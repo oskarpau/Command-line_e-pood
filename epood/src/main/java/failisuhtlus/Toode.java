f667cf59755652dfbaa0e5f6e6e69c1518dd4eb5
jurgenkoosapoeg <jurgen.koosapoeg@gmail.com>
2025-03-26 12:02:31 +0200

    Failist andmete lugemise algprototüüp

---
+++ epood/src/main/java/failisuhtlus/Toode.java
@@ -0,0 +1,39 @@
+public class Toode {
+    private int number;
+    private String nimi;
+    private double hind;
+    private int lao_seis;
+
+    public Toode(int number, String nimi, double hind, int lao_seis) {
+        this.number = number;
+        this.nimi = nimi;
+        this.hind = hind;
+        this.lao_seis = lao_seis;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getNimi() {
+        return nimi;
+    }
+
+    public double getHind() {
+        return hind;
+    }
+
+    public int getLao_seis() {
+        return lao_seis;
+    }
+
+    @Override
+    public String toString() {
+        return "Toode{" +
+                "number=" + number +
+                ", nimi='" + nimi + '\'' +
+                ", hind=" + hind +
+                ", lao_seis=" + lao_seis +
+                '}';
+    }
+}
---
+++ epood/src/main/java/failisuhtlus/jsonReader.java
@@ -0,0 +1,61 @@
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class jsonReader {
+    private String fail;
+
+    public jsonReader(String fail) {
+        this.fail = fail;
+    }
+
+    public List<Toode> getTooted() throws IOException {
+
+        List<Toode> tooted = new ArrayList<>();
+
+        JSONObject json = getJSON();
+
+        JSONArray tootedArray = json.getJSONArray("tooted");
+
+        for (int i = 0; i < tootedArray.length(); i++) {
+            JSONObject toode = tootedArray.getJSONObject(i);
+            tooted.add(new Toode(toode.getInt("tootenumber"), toode.getString("nimi"), toode.getDouble("hind"), toode.getInt("lao seis")));
+        }
+
+        return tooted;
+    }
+
+    public void addToode(Toode toode) throws IOException {
+
+        JSONObject json = getJSON();
+
+        JSONArray tootedArray = json.getJSONArray("tooted");
+
+        JSONObject toodeJson = new JSONObject();
+        toodeJson.put("tootenumber", toode.getNumber());
+        toodeJson.put("nimi", toode.getNimi());
+        toodeJson.put("hind", toode.getHind());
+        toodeJson.put("lao seis", toode.getLao_seis());
+
+        tootedArray.put(toodeJson);
+
+        updateJson(json);
+    }
+
+    private JSONObject getJSON() throws IOException {
+        String content = new String(Files.readAllBytes(Paths.get("andmebaas.json")));
+        return new JSONObject(content);
+    }
+
+    private void updateJson(JSONObject json) throws IOException {
+        try (FileWriter fileWriter = new FileWriter(fail)) {
+            fileWriter.write(json.toString());
+        }
+    }
+}
