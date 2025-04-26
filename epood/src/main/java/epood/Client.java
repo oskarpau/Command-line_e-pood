6b6bcf032de9ff1248b4e464df790ac421591ffa
sammulk <95523661+sammulk@users.noreply.github.com>
2025-04-26 14:48:58 +0300

    port connection uses TLS

--- epood/src/main/java/epood/Client.java
+++ epood/src/main/java/epood/Client.java
@@ -2,8 +2,16 @@ package epood;
 
 // käsurea argumendid: file fail1.txt echo tere kaks korda file error.txt
 
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.TrustManagerFactory;
 import java.io.*;
 import java.net.Socket;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
@@ -13,27 +21,31 @@ public class Client {
     public static void main(String[] args) throws Exception {
 
         System.out.println("connecting to server");
-        try (Socket socket = new Socket("localhost", 1337);
-             DataInputStream din = new DataInputStream(socket.getInputStream());
-             DataOutputStream dout = new DataOutputStream(socket.getOutputStream());
-             Scanner console = new Scanner(System.in)) {
 
-            System.out.println("connected; entering command: ");
-            //suhtlus
-            readServer(din);
-            while (true) {
-                writeServer(console, dout);
-                try {
-                    readServer(din);
-                } catch (EOFException e) { //handler lõpetas töö
-                    System.out.println("closing connection");
-                    break;
-                }
-            }
+        secureConnection(1337); //hangi ühendus TLS-iga
 
-        } finally {
-            System.out.println("Client has finished");
-        }
+        //unsecure connection
+//        try (Socket socket = new Socket("localhost", 1337);
+//             DataInputStream din = new DataInputStream(socket.getInputStream());
+//             DataOutputStream dout = new DataOutputStream(socket.getOutputStream());
+//             Scanner console = new Scanner(System.in)) {
+//
+//            System.out.println("connected; entering command: ");
+//            //suhtlus
+//            readServer(din);
+//            while (true) {
+//                writeServer(console, dout);
+//                try {
+//                    readServer(din);
+//                } catch (EOFException e) { //handler lõpetas töö
+//                    System.out.println("closing connection");
+//                    break;
+//                }
+//            }
+//
+//        } finally {
+//            System.out.println("Client has finished");
+//        }
     }
 
     private static void writeServer(Scanner console, DataOutputStream dout) throws IOException {
@@ -49,52 +61,37 @@ public class Client {
     }
 
 
-    //vanad kodutöö meetodid, ei tea kas kasulik
-    /*
-    public void sendMsg(DataInputStream din, DataOutputStream dout, List<String> msg) throws Exception {
-        dout.writeInt(0);
-        dout.writeInt(msg.size());
-        for (String s : msg) {
-            dout.writeUTF(s);
-        }
-        System.out.println("sent message: " + msg);
-        String resp = din.readUTF();
-        System.out.println("received response: " + resp);
-    }
+    private static void secureConnection(int port) throws IOException, KeyStoreException, CertificateException, NoSuchAlgorithmException, KeyManagementException {
+        File storeFile = new File("epood/truststore.p12");
+        String storePass = "secret";
 
-    public void requestFile(DataInputStream din, DataOutputStream dout, String failinimi) throws IOException {
-        dout.writeInt(1);
-        dout.writeUTF(failinimi);
-        System.out.println("Asked for file: " + failinimi);
-        receiveFile(din, failinimi);
-    }
+        KeyStore store = KeyStore.getInstance(storeFile, storePass.toCharArray());
+        TrustManagerFactory tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+        tmf.init(store);
+        TrustManager[] trustManagers = tmf.getTrustManagers();
 
-    public void receiveFile(DataInputStream din, String failinimi) throws IOException {
-        int bytes = 0;
-        int ok_status = din.readByte();
-        if (ok_status == 6) {
-            System.out.println("ERROR: no file found");
-            return;
-        } else if (ok_status == 7) {
-            System.out.println("ERROR: absolute path was provided");
-            return;
-        } else if (ok_status == 5) {
-            System.out.println("file received from server");
-        } else {
-            System.out.println("ERROR: unknown status " + ok_status);
-            return;
-        }
+        SSLContext ctx = SSLContext.getInstance("TLS");
+        ctx.init(null, trustManagers, null);
+        try (Socket socket = ctx.getSocketFactory().createSocket("localhost", port);
+             DataInputStream din = new DataInputStream(socket.getInputStream());
+             DataOutputStream dout = new DataOutputStream(socket.getOutputStream());
+             Scanner console = new Scanner(System.in)) {
+            // use like a regular socket
 
-        // try with resources
-        try (FileOutputStream fos = new FileOutputStream(path + failinimi)) {
-            long size = din.readLong();
-            byte[] buffer = new byte[4 * 1024];
-            while (size > 0 && (bytes = din.read(buffer, 0, (int) Math.min(buffer.length, size))) != -1) {
-                fos.write(buffer, 0, bytes);
-                size -= bytes;
+            System.out.println("connected; entering command: ");
+            //suhtlus
+            readServer(din);
+            while (true) {
+                writeServer(console, dout);
+                try {
+                    readServer(din);
+                } catch (EOFException e) { //handler lõpetas töö
+                    System.out.println("closing connection");
+                    break;
+                }
             }
-            System.out.println("file saved");
+        } finally {
+            System.out.println("Client has finished");
         }
-    } */
-
+    }
 }
--- epood/src/main/java/epood/Server.java
+++ epood/src/main/java/epood/Server.java
@@ -1,68 +1,53 @@
 package epood;
 
-import failisuhtlus.JsonManagerClient;
-
+import javax.net.ssl.KeyManager;
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.security.*;
+import java.security.cert.CertificateException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Server {
-    String path = "serverFiles" + File.separatorChar;
-    static final byte TYPE_OK = 5;
-    static final byte TYPE_ERROR_NOT_FOUND = 6;
-    static final byte TYPE_ERROR_ABS_PATH = 7;
 
     public static void main(String[] args) throws Exception {
         try (ExecutorService pool = Executors.newCachedThreadPool()) {
             Server server = new Server();
 
-            try (ServerSocket ss = new ServerSocket(1337)) {
-                System.out.println("now listening on :1337");
-                while (true) {
-                    Socket clientsocket = ss.accept();
-                    pool.execute(new ClientHandler(clientsocket, server));
-                }
-            }
-        }
+            secure(server, pool);  //TLS-iga port connection
 
-        //System.out.println("Server pandi kinni, sest serveriga oli üle kahe korra ühendatud");
+//            try (ServerSocket ss = new ServerSocket(1337)) {
+//                System.out.println("now listening on :1337");
+//                while (true) {
+//                    Socket clientsocket = ss.accept();
+//                    pool.execute(new ClientHandler(clientsocket, server));
+//                }
+//            }
+        }
     }
 
 
+    private static void secure(Server server, ExecutorService pool) throws CertificateException, KeyStoreException, IOException, NoSuchAlgorithmException, UnrecoverableKeyException, KeyManagementException {
+        File storeFile = new File("epood/keystore.p12"); //public ja private keyd
+        String storePass = "secret"; //serveri võtme salasõna, peaks genereerima, aga kuna võtmed tulevad cmdlinelt ss las ta olla
 
+        KeyStore store = KeyStore.getInstance(storeFile, storePass.toCharArray());
+        KeyManagerFactory kmf = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+        kmf.init(store, storePass.toCharArray());
+        KeyManager[] keyManagers = kmf.getKeyManagers();
 
-    public void sendFile(DataInputStream din, DataOutputStream dout) throws IOException {
-        int bytes = 0;
-        String failinimi = path + din.readUTF();
-        Path pathFile = Paths.get(failinimi);
-        System.out.print("Sending of file status: ");
-        if (!Files.exists(pathFile)) {
-            dout.writeByte(TYPE_ERROR_NOT_FOUND);
-            System.out.println("TYPE_ERROR_NOT_FOUND");
-            return;
-        } else if (pathFile.isAbsolute()) {
-            dout.writeByte(TYPE_ERROR_ABS_PATH);
-            System.out.println("TYPE_ERROR_ABS_PATH");
-            return;
-        } else { // eeldame, et rohkem erroreid pole
-            dout.writeByte(TYPE_OK);
-            System.out.println("TYPE_OK");
-        }
-        File file = new File(failinimi);
-
-        try(FileInputStream fis = new FileInputStream(file)) {
-            dout.writeLong(file.length());
-            byte[] buffer = new byte[4 * 1024];
-            while ((bytes = fis.read(buffer)) != -1) {
-                dout.write(buffer, 0, bytes);
-                dout.flush();
+        SSLContext ctx = SSLContext.getInstance("TLS");
+        ctx.init(keyManagers, null, null);
+        try (ServerSocket ss = ctx.getServerSocketFactory().createServerSocket(1337)) {
+            // use like a regular server socket
+            System.out.println("now listening on :1337");
+            while (true) {
+                Socket clientsocket = ss.accept();
+                pool.execute(new ClientHandler(clientsocket, server));
             }
-            System.out.println("File sent");
         }
     }
 
